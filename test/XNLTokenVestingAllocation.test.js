9249554b4ca99dd9cad30a761ed2ac906c23f93a
Marek Tlacbaba <mtlacbaba@users.noreply.github.com>
2021-09-15 10:41:32 +0800

    extended test to cover new vesting schemes

--- test/XNLTokenVestingAllocation.test.js
+++ test/XNLTokenVestingAllocation.test.js
@@ -1,10 +1,14 @@
 const XNL = artifacts.require("XNLToken");
 const BN = require('bn.js');
+const addDays = require('date-fns/addDays');
 
 require('chai')
   .use(require('chai-as-promised'))
   .use(require('chai-bn')(BN))
   .should();
+
+const epoch = new Date('1970-1-1')
+const dateTGE = Math.floor(new Date('2021-09-16')/8.64e7);
 
 
 contract('XNLTokenVesting', function ([owner, account1, account2, account3, account4, account5, account6, account7, account8, account9, _]) {
@@ -13,24 +17,26 @@ contract('XNLTokenVesting', function ([owner, account1, account2, account3, acco
 
   const checkBalance = async (token, address, expected) => {
     let balance = await token.balanceOf(address);
+    let res = await token.vestingAsOf(dateTGE, { from: address });
+    console.log('BalanceOf [', address, ']: [', web3.utils.fromWei(balance), '], notVested: [', web3.utils.fromWei(res.amountNotVested), '], vested: [', web3.utils.fromWei(res.amountVested), ']')
     // console.log('BalanceOf [', address, '] with expected [', expected.toString(), ']: ', balance.toString())
     balance.should.be.bignumber.equal(expected);
   }
 
   const checkVesting = async (token, address, day, expected) => {
     let res = await token.vestingAsOf(day, { from: address });
-    // console.log(`${day}, ${day - dateTGE}, ${res.amountVested.toString()}, ${expected.toString()}`)
+    
+    console.log(`${addDays(epoch, day).toISOString().split('T')[0]}, ${day - dateTGE}, ${web3.utils.fromWei(res.amountVested)}, ${web3.utils.fromWei(res.amountNotVested)}`)
     res.amountVested.should.be.bignumber.equal(expected);
   }
 
   const checkVestingApproximatly = async (token, address, day, expectedMin, expectedMax) => {
     let res = await token.vestingAsOf(day, { from: address });
-    // console.log(`${day}, ${day - dateTGE}, ${res.amountVested.toString()}, ${expectedMin.toString()}, ${expectedMax.toString()}`)
+    console.log(`${day}, ${day - dateTGE}, ${res.amountVested.toString()}, ${expectedMin.toString()}, ${expectedMax.toString()}`)
     res.amountVested.should.be.bignumber.least(expectedMin);
     res.amountVested.should.be.bignumber.most(expectedMax);
   }
 
-  const dateTGE = Math.floor(new Date()/8.64e7);
 
   const zero = new BN(0);
 
@@ -88,6 +94,164 @@ contract('XNLTokenVesting', function ([owner, account1, account2, account3, acco
       }
     }
   });
+
+  it("...seed round tokens (11,200,000) are distibuted 15% unlocked and then 15% released every 3 months and in the end 10% manually", async function () {
+    const seed1 = new BN(`9000000${ZEROS}`)
+    const seed1Vested = new BN(`750000${ZEROS}`)
+    const seed1Interval1 = new BN(`150000000000000000000000`);
+    const seed1Interval2 = new BN(`300000000000000000000000`);
+    const seed1Interval3 = new BN(`450000000000000000000000`);
+    const seed1Interval4 = new BN(`600000000000000000000000`);
+    const seed1Interval5 = new BN(`750000000000000000000000`);
+
+    const seed2 = new BN(`1199999700000000000000000`)
+    const seed2Vested = new BN(`999999750000000000000000`)
+    const seed2Interval1 = new BN(`199999950000000000000000`);
+    const seed2Interval2 = new BN(`399999900000000000000000`);
+    const seed2Interval3 = new BN(`599999850000000000000000`);
+    const seed2Interval4 = new BN(`799999800000000000000000`);
+    const seed2Interval5 = new BN(`999999750000000000000000`);
+
+    await this.token.grantVestingTokens(
+      account1, seed1, seed1Vested, dateTGE, 450, 0, 90,
+      { from: owner }
+    );
+    await this.token.grantVestingTokens(
+      account2, seed2, seed2Vested, dateTGE, 450, 0, 90,
+      { from: owner }
+    );
+
+    await checkBalance(this.token, account1, seed1);
+    await checkBalance(this.token, account2, seed2);
+
+    for (let index = 0; index < 451; index++) {
+      if (index < 90) {
+        await checkVesting(this.token, account1, dateTGE + index, zero);
+        await checkVesting(this.token, account2, dateTGE + index, zero);
+      } else if (index < 180) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval1);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval1);
+      } else if (index < 270) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval2);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval2);
+      } else if (index < 360) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval3);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval3);
+      } else if (index < 450) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval4);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval4);
+      } else {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval5);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval5);
+      } 
+    }
+  });
+
+  it("...seed round tokens (11,200,000) are distibuted 15% unlocked and then 14.166666666666% released every 3 months", async function () {
+    const seed1 = new BN(`1000000${ZEROS}`)
+    const seed1Vested = new BN(`850000${ZEROS}`)
+    const seed1Interval1 = new BN(`141666666666666666666666`);
+    const seed1Interval2 = new BN(`283333333333333333333333`);
+    const seed1Interval3 = new BN(`425000000000000000000000`);
+    const seed1Interval4 = new BN(`566666666666666666666666`);
+    const seed1Interval5 = new BN(`708333333333333333333333`);
+    const seed1Interval6 = new BN(`850000${ZEROS}`);
+
+    const seed2 = new BN(`1333333${ZEROS}`)
+    const seed2Vested = new BN(`1133333050000000000000000`)
+    const seed2Interval1 = new BN(`188888841666666666666666`);
+    const seed2Interval2 = new BN(`377777683333333333333333`);
+    const seed2Interval3 = new BN(`566666525000000000000000`);
+    const seed2Interval4 = new BN(`755555366666666666666666`);
+    const seed2Interval5 = new BN(`944444208333333333333333`);
+    const seed2Interval6 = new BN(`1133333050000000000000000`);
+
+    await this.token.grantVestingTokens(
+      account1, seed1, seed1Vested, dateTGE, 540, 0, 90,
+      { from: owner }
+    );
+    await this.token.grantVestingTokens(
+      account2, seed2, seed2Vested, dateTGE, 540, 0, 90,
+      { from: owner }
+    );
+
+    await checkBalance(this.token, account1, seed1);
+    await checkBalance(this.token, account2, seed2);
+
+    for (let index = 0; index < 541; index++) {
+      if (index < 90) {
+        await checkVesting(this.token, account1, dateTGE + index, zero);
+        await checkVesting(this.token, account2, dateTGE + index, zero);
+      } else if (index < 180) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval1);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval1);
+      } else if (index < 270) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval2);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval2);
+      } else if (index < 360) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval3);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval3);
+      } else if (index < 450) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval4);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval4);
+      } else if (index < 540) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval5);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval5);
+      } else {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval6);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval6);
+      }
+    }
+  });
+
+
+  it("....private round tokens (21,000,000) are distibuted 20% unlocked and then 20% released every 3 months", async function () {
+    const seed1 = new BN(`1000000${ZEROS}`)
+    const seed1Vested = new BN(`800000${ZEROS}`)
+    const seed1Interval1 = new BN(`200000${ZEROS}`);
+    const seed1Interval2 = new BN(`400000${ZEROS}`);
+    const seed1Interval3 = new BN(`600000${ZEROS}`);
+    const seed1Interval4 = new BN(`800000${ZEROS}`);
+
+    const seed2 = new BN(`1333333${ZEROS}`)
+    const seed2Vested = new BN(`1066666400000000000000000`)
+    const seed2Interval1 = new BN(`266666600000000000000000`);
+    const seed2Interval2 = new BN(`533333200000000000000000`);
+    const seed2Interval3 = new BN(`799999800000000000000000`);
+    const seed2Interval4 = new BN(`1066666400000000000000000`);
+
+    await this.token.grantVestingTokens(
+      account1, seed1, seed1Vested, dateTGE, 360, 0, 90,
+      { from: owner }
+    );
+    await this.token.grantVestingTokens(
+      account2, seed2, seed2Vested, dateTGE, 360, 0, 90,
+      { from: owner }
+    );
+
+    await checkBalance(this.token, account1, seed1);
+    await checkBalance(this.token, account2, seed2);
+
+    for (let index = 0; index < 361; index++) {
+      if (index < 90) {
+        await checkVesting(this.token, account1, dateTGE + index, zero);
+        await checkVesting(this.token, account2, dateTGE + index, zero);
+      } else if (index < 180) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval1);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval1);
+      } else if (index < 270) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval2);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval2);
+      } else if (index < 360) {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval3);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval3);
+      } else {
+        await checkVesting(this.token, account1, dateTGE + index, seed1Interval4);
+        await checkVesting(this.token, account2, dateTGE + index, seed2Interval4);
+      }
+    }
+  });
+
 
   it("..partnership tokens (10,000,000) are distibuted 6 month cliff, then 20% released every 3 months", async function () {
     const allocated = new BN('15000000000000000000000000');
